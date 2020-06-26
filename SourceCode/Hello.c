aedf35753e1dec79d8a34098d43a67e474b3fd90
Pragya Tripathi <pragya03099@gmail.com>
2020-06-26 10:38:33 +0530

    Here using inverse patch usless changes reverted back Manually removed the changes from Hello.c file and then again add commit and push another method exists which is revert last commit

--- SourceCode/Hello.c
+++ SourceCode/Hello.c
@@ -6,12 +6,8 @@ void fun()
 {
 	printf("How you doing");
 }
-void fun1(){
-	printf("Created another brach and making changes in that branch");
-}
 int main()
 {
-	fun1();
 	hello();
 	fun();
 	return 0;
